8feb24a671fd878b6097c4a78462d93b18a08876
Bruno <bruno.pasquini@endava.com>
2024-07-26 10:35:37 -0300

    Added subtitle

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class AppConstants {
     public apiURL = 'http://localhost:5010/api/Article/GetLatest?page=';
-    public appTitle = "News Articles";
+    public appTitle = "Tech News Hub";
+    public appSubtitle = "Stay Informed with the Latest Trends, Innovations, and Insights in the Tech World";
     public listTitle = "List of Articles";
     public loading = "Loading...";
     public urlError = "URL not found";
